65a24882c8648b2e86329de38a592abd296c5df4
Kai <m3sv.is.me@gmail.com>
2021-02-04 18:33:14 +1100

    Updates

---
+++ precache-manifest.e93df9adbf5683bff6ac3fa4508c963c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef94724f2b4e03eb097dab88dc43d32c",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "40c802c371c874ec4dcd",
+    "url": "/react-todo-app/static/css/main.c303ebc9.chunk.css"
+  },
+  {
+    "revision": "aa4eea019a63e88ec54c",
+    "url": "/react-todo-app/static/js/2.005fa5d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.005fa5d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c802c371c874ec4dcd",
+    "url": "/react-todo-app/static/js/main.cf682e52.chunk.js"
+  },
+  {
+    "revision": "923cf4596e411f727a39",
+    "url": "/react-todo-app/static/js/runtime-main.8a1c512e.js"
+  }
+]);
